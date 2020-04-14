343f208cd831d2d5fe9f78d6f929353cf98e8266
bimlas <bimbalaszlo@gmail.com>
2020-04-14 20:54:06 +0200

    Use Redshift's "Night" period instead of "Daytime"
    Daytime -> Transition -> Night

--- xfce4-night-mode-redshift.sh
+++ xfce4-night-mode-redshift.sh
@@ -4,14 +4,14 @@
 # https://gitlab.com/bimlas/xfce4-night-mode (main repository)
 # https://github.com/bimlas/xfce4-night-mode (mirror, please star if you like the plugin)
 
-if ( LC_ALL='C' redshift -vo 2> /dev/null | grep "Period: Daytime" > /dev/null ); then
-  mode='day'
-else
+if ( LC_ALL='C' redshift -vo 2> /dev/null | grep 'Period: Night' > /dev/null ); then
   mode='night'
+else
+  mode='day'
 fi
 
-"$(dirname "$0")/xfce4-night-mode.sh" $mode | sed '/<tool>/,/<\/tool>/ d'
-echo "<tool>
+"$(dirname "$0")/xfce4-night-mode.sh" "$mode" | sed '/<tool>/,/<\/tool>/ d'
+echo '<tool>
   Night mode defined by RedShift
   Click to toggle mode for a while
-  </tool>"
+  </tool>'
