6c8fbe773e01a8d6d6155a24af78f03e0c0dd1d4
bimlas <bimbalaszlo@gmail.com>
2020-04-15 07:15:06 +0200

    Fix flickering of Redshift controlled night time
    When we queried Redshift for the current time of day, the screen became
    even redder for a short time because it wasn’t using the right switches.

--- xfce4-night-mode-redshift.sh
+++ xfce4-night-mode-redshift.sh
@@ -4,7 +4,7 @@
 # https://gitlab.com/bimlas/xfce4-night-mode (main repository)
 # https://github.com/bimlas/xfce4-night-mode (mirror, please star if you like the plugin)
 
-if ( LC_ALL='C' redshift -vo 2> /dev/null | grep 'Period: Night' > /dev/null ); then
+if ( LC_ALL='C' redshift -p 2> /dev/null | grep 'Period: Night' > /dev/null ); then
   mode='night'
 else
   mode='day'
